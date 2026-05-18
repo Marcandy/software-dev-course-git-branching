c46d2cc483d94fea4894120bc84ee56997fbfc5a
Marcandy <marcandy.nj@gmail.com>
2026-05-18 09:51:39 -0400

    added even logic to loop

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ for (let i = 0; i < 10; i++) {
     // muliply i by 2
     let double = i * 2;
     console.log(double);
+    // checking for even number
+    if(i % 2 === 0) {
+        console.log('i is even')
+    }
 }
